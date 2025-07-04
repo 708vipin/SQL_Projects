848f5f5fe4ee9ce713bef610ba71c224cf5fdb89
Vipin Kumar Pandey <pvipin708@gmail.com>
2025-07-04 07:34:32 -0700

    Add files via upload

---
+++ Problem_Statement.sql
@@ -0,0 +1,25 @@
+/*   BASIC QUERIES
+1. Retrieve all books in the "Fiction" genre.
+2. Find the books published after the year 1950.
+3. List all customers from the Canada.
+4. Show Orders placed in November 2023.
+5. Retrieve the total stock of books available.
+6. Find the details of the most expensive book.
+7. Show all customers who ordered more than 1 quantity of a book.
+8. Retrieve all the orders where the total amount exceeds $20.
+9. List all genres available in the Books table.
+10. Find the books with the lowest stock.
+11. Calculate the total revenue generated from all the orders.
+*/
+
+/*    ADVANCED QUERIES
+1. Retrieve the total number of books sold for each genre.
+2. Find the average price of books in the "Fantasy genre".
+3. List customers who have placed at least 2 orders.
+4. Find the most frequently ordered book.
+5. Show the top 3 most expensive books of "Fantasy" genre.
+6. Retrieve the total quantity of books sold by each author.
+7.List the cities where customers who spent over $30 are located.
+8. Find the customers who spent the most on orders.
+9. Calculate the stock remaining after fulfilling all orders.
+*/
